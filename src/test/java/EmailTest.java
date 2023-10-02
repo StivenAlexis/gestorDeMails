72b311e5560c9b17af4170c40fd9dcf92b62ed15
Stiven Alexis <stiven22monsalvo@gmail.com>
2023-10-02 16:16:25 -0300

    update

--- src/test/java/EmailTest.java
+++ src/test/java/EmailTest.java
@@ -8,17 +8,20 @@ import gestor.Email;
 
 public class EmailTest {
     
+
     @Test
     public void emailValid(){
 
         Email e1 = new Email();
         Contact c1 = new Contact("Juan", "martinez", "Stiven22@gmail.comn");
         Contact c2 = new Contact("Juan", "martinez", "Stiven22@gmail.comn");
-
+        
+        e1.subject= "no se que poner";
         e1.content = "hola";
         e1.sender= c1;
         e1.to.add(c2);
 
+        assertNotEquals("",e1.subject);
         assertNotEquals(0,e1.to.size());
         assertNotEquals("",e1.content);
         assertNotEquals(null,e1.sender);  
