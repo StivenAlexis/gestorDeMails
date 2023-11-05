d722ff29d116e6dd9ba31af79404e21be2488952
Stiven Alexis <stiven22monsalvo@gmail.com>
2023-11-05 12:42:15 -0300

    completed

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -248,6 +248,85 @@ public class FilterTest {
 
         assertEquals(1,filteredEmails.size());
     }
+
+    @Test
+    public void FinalTest(){
+        
+    ArrayList<Email> emails = new ArrayList<>();
+        Contact c1 = new Contact("Stiven@ucp.com");
+        Contact c2 = new Contact("alejoEs@ucp.com");
+
+        for (int i = 0; i <50; i++) {
+
+            emails.add( new Email(c1,c2));     
+            emails.get(i).setSubject("success");
+
+        } 
+
+        for (int i= 0 ; i <51; i++) {
+
+            emails.add( new Email(c1,c2));     
+            emails.get(i).setSubject("desaprobado");
+            
+        } 
+
+        
+        assertEquals(101,emails.size());
+        
+
+       
+
+        List<Email> filteredEmails = Filter.filterEmails(emails , Filter.FindASenderWhithASubject("Stiven@ucp.com","desaprobado"));
+        
+        assertEquals(51,filteredEmails.size());
+
+        
+       
+
+       
+
+        
+        /* 
+        MailBox ma1 = new MailBox(c1.getEmailAddress());
+        MailBox ma2 = new MailBox(c2.getEmailAddress());
+        
+
+        ArrayList<MailBox> mailBoxes = new ArrayList<MailBox>();
+        mailBoxes.add(ma1);
+        mailBoxes.add(ma2);
+
+        Manager m1= new Manager(mailBoxes);
+
+        m1.sort(e1);
+        m1.send(e1);
+        
+        //corroboramos que el email esta cada bandeja
+
+        assertEquals(e1,ma1.getTrays().getOutbox().get(0));
+
+        assertEquals(e1,ma2.getTrays().getInbox().get(0));
+
+        assertEquals(e1,ma3.getTrays().getInbox().get(0));
+
+        m1.sort(e2);
+        m1.send(e2);
+        
+
+        //corroboramos que el email esta cada bandeja 
+        
+        assertEquals(e2,ma2.getTrays().getOutbox().get(0));
+
+        assertEquals(e2,ma1.getTrays().getInbox().get(0));
+
+        assertEquals(e2,ma3.getTrays().getInbox().get(1));
+        */
+    }
+
+    private void asserEquals(String string, String subject) {
+    }
+
+    
     
 }
+    
 
